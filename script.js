6d2d23896518ed8bcd2938488e2fb2308056f71c
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-25 21:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+var fname=document.getElementById(fname).value
+	var lname=document.getElementById(lname).value
+	var message="First name:"+fname+"Last name:"+lname;
+	alert(message);
 }
