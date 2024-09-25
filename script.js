00d4371c08c83a7f8673fc55f53a82ae22cd500d
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-25 22:00:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
-var fname=document.getElementById(fname).value;
-	var lname=document.getElementById(lname).value;
+var fname=document.getElementById(fname)[0].value;
+	var lname=document.getElementById(lname)[0].value;
 	alert("First name:"+fname+"Last name:"+lname)
 	
 }
